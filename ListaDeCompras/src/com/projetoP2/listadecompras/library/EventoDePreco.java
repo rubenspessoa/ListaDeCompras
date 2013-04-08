24628b6e27373a17dbdfebd4c2094e0d397d9c70
Rubens Pessoa de Barros Filho <pessoadebarros@gmail.com>
2013-04-08 17:57:40 -0300

    Evento de Preço

--- ListaDeCompras/src/com/projetoP2/listadecompras/library/EventoDePreco.java
+++ ListaDeCompras/src/com/projetoP2/listadecompras/library/EventoDePreco.java
@@ -1,7 +1,12 @@
 package com.projetoP2.listadecompras.library;
 
 import java.io.Serializable;
+<<<<<<< HEAD
 import java.util.Date;
+=======
+import java.util.Calendar;
+
+>>>>>>> Evento de Preço
 public class EventoDePreco implements Serializable{
 	
 	/**
@@ -9,17 +14,28 @@ public class EventoDePreco implements Serializable{
 	 */
 	private static final long serialVersionUID = 2469973788231849455L;
 	
+<<<<<<< HEAD
 	String estabelecimento; //Generico (Implementar mais cuidadosamente depois :P)
+=======
+	Calendar data;
+	String estabelecimento;
+>>>>>>> Evento de Preço
 	double valor;
 	Date data;
 
+<<<<<<< HEAD
 	public EventoDePreco(Date data, String estabelecimento, double valor) {
 		this.data = data;
+=======
+	public EventoDePreco(double valor, String estabelecimento) {
+		data = Calendar.getInstance();
+>>>>>>> Evento de Preço
 		this.estabelecimento = estabelecimento;
 		this.valor = valor;
 	}
 
 
+<<<<<<< HEAD
 	public Date getData() {
 		return this.data;
 	}
@@ -30,6 +46,12 @@ public class EventoDePreco implements Serializable{
 	}
 
 
+=======
+	public Calendar getData() {
+		return data;
+	}
+
+>>>>>>> Evento de Preço
 	public String getEstabelecimento() {
 		return estabelecimento;
 	}
@@ -48,6 +70,10 @@ public class EventoDePreco implements Serializable{
 	public void setValor(double valor) {
 		this.valor = valor;
 	}
-	
+
+
+	public int compareTo(Calendar anotherCalendar) {
+		return data.compareTo(anotherCalendar);
+	}
 	
 }
