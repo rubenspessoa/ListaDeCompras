7e66da56d923e108aa693c24e5c55f1c7708bbfa
Rubens Pessoa de Barros Filho <pessoadebarros@gmail.com>
2013-04-08 22:04:22 -0300

    Tirei os imports

--- ListaDeCompras/src/com/projetoP2/listadecompras/library/ListaDeCompras.java
+++ ListaDeCompras/src/com/projetoP2/listadecompras/library/ListaDeCompras.java
@@ -1,10 +1,8 @@
 package com.projetoP2.listadecompras.library;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Date;
-import java.util.Set;
 
 public class ListaDeCompras implements Serializable {
 
