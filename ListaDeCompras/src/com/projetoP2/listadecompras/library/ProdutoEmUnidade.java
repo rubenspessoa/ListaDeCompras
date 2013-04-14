4eb9a2a6b53909522a2bb49be119d84a855f7191
Rubens Pessoa de Barros Filho <pessoadebarros@gmail.com>
2013-04-13 21:03:11 -0300

    ProdutoEmUnidade

--- ListaDeCompras/src/com/projetoP2/listadecompras/library/ProdutoEmUnidade.java
+++ ListaDeCompras/src/com/projetoP2/listadecompras/library/ProdutoEmUnidade.java
@@ -9,6 +9,12 @@ public class ProdutoEmUnidade extends Produto implements Medida, Serializable {
 	 */
 	private static final long serialVersionUID = -580427383168426924L;
 
+	public ProdutoEmUnidade(String nome, String estabelecimento, double valor) {
+		super(nome, estabelecimento, valor);
+		int quantidade = 1;
+		addEventoDePreco(calculaValorDaUnidadeDeMedida(quantidade, valor), estabelecimento);
+	}
+	
 	public ProdutoEmUnidade(String nome, String estabelecimento, int quantidade, double valor) {
 		super(nome, estabelecimento, valor);
 		addEventoDePreco(calculaValorDaUnidadeDeMedida(quantidade, valor), estabelecimento);
