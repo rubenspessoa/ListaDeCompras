d620becaad9ace049e7994a47f9c7e9ff40e502b
Rubens Pessoa de Barros Filho <pessoadebarros@gmail.com>
2013-04-18 20:51:56 -0300

    Criando produtos genéricos automaticamente e refatorando.

--- ListaDeCompras/src/com/projetoP2/listadecompras/library/Medida.java
+++
@@ -1,8 +0,0 @@
-package com.projetoP2.listadecompras.library;
-
-public interface Medida {
-
-	public double calculaValor(double quantidade);
-	public double calculaValorDaUnidadeDeMedida(double quantidade, double preco);
-	
-}
